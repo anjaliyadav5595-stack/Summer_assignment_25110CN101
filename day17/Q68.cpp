5677b0d93288efe4a5d9bd7b4fd2df46257b72e6
ANJALI YADAV <anjali.yadav5595@gmail.com>
2026-06-30 23:05:59 +0530

    day 17

---
+++ day17/Q68.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;cin>>n>>m;
+    int a[100],b[100];
+    for(int i=0;i<n;i++)cin>>a[i];
+    for(int i=0;i<m;i++)cin>>b[i];
+    cout<<"commom elements:";
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(a[i]==b[j]){
+                cout<<a[i]<<" ";
+                break;
+            }
+        }
+    }
+}
