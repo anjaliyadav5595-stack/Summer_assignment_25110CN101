b03fb89b2092c0231a588cb2f3d29838a5cd5129
ANJALI YADAV <anjali.yadav5595@gmail.com>
2026-06-30 23:10:42 +0530

    day 18

--- day18/Q65.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-int main(){
-    int n,n;cin>>n>>m;vector<int>a(n+m);
-    for(int i=0;i<n;i++)cin>>a[i];
-    for(int i=0;i<m;i++)cin>>a[n+i];
-    for(int x:a)cout<<x<<" ";
-}
--- day18/Q66.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<set>
-using namespace std;
-int main(){
-    int n,m;cin>>n>>m;set<int>s;
-    for(int i=0;x;i<n+m;i++){cin>>x;s.insert(x);}
-    for(int x:s)cout<<x<<" ";
-}
-
